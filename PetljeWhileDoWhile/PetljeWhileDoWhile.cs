4142df555d5f47ca91729f6fdeb91d47be270ad2
julijan <julijan.sribar@gmail.com>
2015-11-05 18:11:42 +0100

    Varaible renamed and pulled outside the loop

--- PetljeWhileDoWhile/PetljeWhileDoWhile.cs
+++ PetljeWhileDoWhile/PetljeWhileDoWhile.cs
@@ -9,21 +9,22 @@ namespace Vsite.CSharp
             Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
 
             int brojBacanja = 0;
+            int bacanje = 0;
             // TODO: Napisati simulaciju početka igre "Čovječe ne ljuti se": kocka se baca sve dok ne padne na 6
 
             {
                 ++brojBacanja;
-                int slučajniBroj = generatorSlučajnih.Next(1, 6); // generira slučajni broj između 1 i 6
-                Console.WriteLine("{0}. pokušaj: {1}", brojBacanja, slučajniBroj);
+                bacanje = generatorSlučajnih.Next(1, 6); // generira slučajni broj između 1 i 6
+                Console.WriteLine("{0}. pokušaj: {1}", brojBacanja, bacanje);
             }
 
             int brojPređenihPolja = brojBacanja;
             // TODO: Napisati petlju u kojoj se kocka baca sve dok brojPređenihPolja ne postane veći od 12
 
             {
-                int slučajniBroj = generatorSlučajnih.Next(1, 6); // generira slučajni broj između 1 i 6
-                Console.WriteLine("{0} + {1}", brojPređenihPolja + slučajniBroj);
-                brojPređenihPolja += slučajniBroj;
+                bacanje = generatorSlučajnih.Next(1, 6); // generira slučajni broj između 1 i 6
+                Console.WriteLine("{0} + {1}", brojPređenihPolja + bacanje);
+                brojPređenihPolja += bacanje;
             }
 
             Console.WriteLine("Zbroj = {0}", brojPređenihPolja);
