9620cfd9012615bffe29c762de4d4281371ab858
Julijan Šribar <julijan.sribar@gmail.com>
2016-10-16 19:55:06 +0200

    Promijenjena klasa PetljeWhileDoWhile i dodani testovi

--- PetljeWhileDoWhile/PetljeWhileDoWhile.cs
+++ PetljeWhileDoWhile/PetljeWhileDoWhile.cs
@@ -2,15 +2,15 @@
 
 namespace Vsite.CSharp
 {
-    class PetljeWhileDoWhile
+    public class PetljeWhileDoWhile
     {
-        static void Main(string[] args)
+        public static int BrojBacanjaDoBačeneŠestice()
         {
             Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
-
             int brojBacanja = 0;
             int bacanje = 0;
-            // TODO: Napisati simulaciju početka igre "Čovječe ne ljuti se": kocka se baca sve dok ne padne na 6
+
+            // TODO: Napisati petlju do-while koja ispisuje rezultat bacanja sve dok ne padne na 6
 
             {
                 ++brojBacanja;
@@ -18,21 +18,34 @@ namespace Vsite.CSharp
                 Console.WriteLine("{0}. pokušaj: {1}", brojBacanja, bacanje);
             }
 
-            int brojPređenihPolja = brojBacanja;
-            // TODO: Napisati petlju u kojoj se kocka baca sve dok brojPređenihPolja ne postane veći od 12
-
-            {
-                bacanje = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
-                Console.WriteLine("{0} + {1}", brojPređenihPolja + bacanje);
-                brojPređenihPolja += bacanje;
-            }
-
-            Console.WriteLine("Zbroj = {0}", brojPređenihPolja);
-
-            Console.ReadKey();
-
-            // TODO: pokrenite program, provjerite njegovu ispravnost i ispis prekopirajte u datoteku "PetljeWhileDoWhile.txt" koja je pridružena ovom projektu
+            return brojBacanja;
         }
 
+        public static int BacajDokNeProđe12Polja(int brojPređenihPolja)
+        {
+            Random generatorSlučajnih = new Random(); // generator slučajnih brojeva
+
+            // TODO: Napisati petlju while koja se ponavlja sve dok brojPređenihPolja ne postane jednak ili veći od 12
+
+            {
+                int bacanje = generatorSlučajnih.Next(1, 7); // generira slučajni broj između 1 i 6
+                Console.WriteLine($"Bacanje: {bacanje}");
+                brojPređenihPolja += bacanje;
+                Console.WriteLine($"Ukupno: {brojPređenihPolja}");
+            }
+            return brojPređenihPolja;
+        }
+
+        static void Main(string[] args)
+        {
+
+            int brojPređenihPolja = BrojBacanjaDoBačeneŠestice();
+
+            brojPređenihPolja = BacajDokNeProđe12Polja(brojPređenihPolja);
+            Console.WriteLine("Zbroj = {0}", brojPređenihPolja);
+
+            Console.WriteLine("GOTOVO!!!");
+            Console.ReadKey();
+        }
     }
 }
---
+++ Testovi/TestPetljeWhileDoWhile.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Vsite.CSharp;
+
+namespace Vsite.CSharp.Testovi
+{
+    [TestClass]
+    public class TestPetljeWhileDoWhile : ConsoleTest
+    {
+        [TestMethod]
+        public void TestPetljeWhileDoWhile_BrojBacanjaDoBačeneŠestice()
+        {
+            PetljeWhileDoWhile.BrojBacanjaDoBačeneŠestice();
+            Assert.IsFalse(cw.IsEmpty); // barem jedno bacanje!
+
+            int n = 1;
+            while (!cw.IsEmpty)
+            {
+                string unos = cw.GetString();
+                string početak = $"{n}. pokušaj: ";
+                Assert.IsTrue(unos.StartsWith(početak));
+                string broj = unos.Substring(početak.Length);
+                int i = int.Parse(broj);
+                Assert.IsTrue(i >= 1 && (i < 6 || (i == 6 && cw.IsEmpty)));
+                ++n;
+            }
+        }
+
+        [TestMethod]
+        public void TestPetljeWhileDoWhile_BacajDokNeProđe12Polja()
+        {
+            int početno = 8;
+            int ukupno = PetljeWhileDoWhile.BacajDokNeProđe12Polja(početno);
+            Assert.IsTrue(ukupno >= 12 && ukupno < 18);
+            Assert.IsFalse(cw.IsEmpty);
+
+            while (!cw.IsEmpty)
+            {
+                string unos = cw.GetString();
+                Assert.IsTrue(unos.StartsWith("Bacanje: "));
+                string broj = unos.Substring("Bacanje: ".Length);
+                int i = int.Parse(broj);
+                Assert.IsTrue(i >= 1 && i < 6);
+                unos = cw.GetString();
+                Assert.IsTrue(unos.StartsWith("Ukupno: "));
+                broj = unos.Substring("Ukupno: ".Length);
+                i = int.Parse(broj);
+                Assert.IsTrue(i >= početno && (i < 12 || cw.IsEmpty));
+            }
+        }
+
+        [TestMethod]
+        public void TestPetljeWhileDoWhile_BacajDokNeProđe12PoljaJePrazanZa12iliViše()
+        {
+            Assert.AreEqual(12, PetljeWhileDoWhile.BacajDokNeProđe12Polja(12));
+            Assert.IsTrue(cw.IsEmpty); // ne smije biti niti jedno bacanje!
+
+            Assert.AreEqual(16, PetljeWhileDoWhile.BacajDokNeProđe12Polja(16));
+            Assert.IsTrue(cw.IsEmpty); // ne smije biti niti jedno bacanje!
+        }
+    }
+}
