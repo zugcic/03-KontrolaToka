033764d379c7bfa896799133d0127216d9e80da5
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-20 20:24:05 +0200

    Riješili smo uvjetno pridruživanje

--- UvjetnoPridruživanje/IspišiParnost.cs
+++ UvjetnoPridruživanje/IspišiParnost.cs
@@ -6,9 +6,8 @@ namespace Vsite.CSharp
     {
         public static void UvjetnimPridruživanjem(int broj)
         {
-            // TODO: Napisati uvjetno pridruživanje ?: koje će ovisno o dijeljivosti 
-            // broja varijabli odgovor dodijeliti vrijednost "paran" ili "neparan"
-            string odgovor = "neparan";
+            
+            string odgovor = broj%2==0 ? "paran": "neparan"; //uvjetno pridruživanje-> uvjet?a ko je: ako nije;
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
@@ -18,6 +17,10 @@ namespace Vsite.CSharp
             // TODO: Korištenjem grananja if dodijeliti varijabli odgovor vrijednost 
             // "paran" ili "neparan" ovisno o djeljivosti zadanog broja
             string odgovor = "neparan";
+            if (broj % 2 == 0)
+            {
+                odgovor = "paran";
+            }
 
             Console.WriteLine("Broj {0} je {1}", broj, odgovor);
         }
