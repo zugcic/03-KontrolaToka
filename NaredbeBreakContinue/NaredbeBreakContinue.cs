fadf8c41cee8da81f35997a2650ec96b538ceac1
Stjepan Zugcic <stzugcic@vsite.hr>
2016-10-20 21:06:28 +0200

    Riješili smo naredbe-break, continue

--- NaredbeBreakContinue/NaredbeBreakContinue.cs
+++ NaredbeBreakContinue/NaredbeBreakContinue.cs
@@ -9,7 +9,9 @@ namespace Vsite.CSharp
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO: Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje naredbom continue)
+               
+                if (i % d != 0)
+                    continue;
 
                 zbroj += i;
             }
@@ -21,11 +23,13 @@ namespace Vsite.CSharp
             int zbroj = 0;
             for (int i = 1; i <= n; ++i)
             {
-                // TODO: Ako i nije djeljiv s d, ne dodaje se (tj. treba se vratiti na početak petlje)
+               
+                if (i % d != 0)
+                    continue;
 
-
-                // TODO: Ako je i djeljiv sa d2, tada treba prekinuti petlju
-
+           
+                if (i % d2 == 0)
+                    break;
 
                 zbroj += i;
             }
